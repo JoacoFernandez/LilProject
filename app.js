75f93904ac16f68f03a8ba7f80aacb98adaa0104
Joaquin Fernandez Abadie <jfernandezabadie@udesa.edu.ar>
2020-02-20 13:17:12 -0300

    Ul for selected artists and clear task button where added Generate a new html for the actual game

--- app.js
+++ app.js
@@ -11,11 +11,55 @@ function findArtist() {
         happi.getArtist(artistText).then(function(value){
             let output = '';
             value.profile.result.forEach(function(artistInfo){
-                 output += `<li class="list-group-item list-group-item-info">Artist Name:    ${artistInfo.artist},         ID:    ${artistInfo.id_artist}, <img src="${artistInfo.cover}" height="42" width="42"></li>`
+                 output += `<li class="list-group-item " id="artistResults" >Artist Name:    ${artistInfo.artist},         ID:    ${artistInfo.id_artist}, <img src="${artistInfo.cover}" height="42" width="42"></li>`
             });
             document.getElementById('artists').innerHTML = output;
         }, function(reason){
 
         })
     }
+}
+
+/* const select = document.getElementById('artistResults');
+const addButton = document.getElementById('addButton');
+const artistList = document.querySelector('.collection'); */
+
+/* loadEventListeners();
+function loadEventListeners(){
+    select.addEventListener('click', selectArtist);
+    addButton.addEventListener('click', addArtist);
+}
+
+function selectArtist(e){
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(artistList));
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content'
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+    artistList.appendChild(li);
+    e.preventDefault()
+} */
+
+document.getElementById('artists').addEventListener('click', selectArtist);
+function selectArtist(e){
+    if (e.target.className.indexOf("active") === -1) {
+        e.target.className = 'selected list-group-item list-group-item-info active';
+    } else {
+        e.target.className = 'list-group-item'
+    }
+}
+const ul = document.querySelector('.collection');
+document.getElementById('addButton').addEventListener('click', addArtist);
+function addArtist() {
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.querySelector('.active'));
+    ul.appendChild(li);
+}
+const artistLi = document.querySelector('.collection-item');
+document.getElementById('clearButton').addEventListener('click', clearArtists);
+function clearArtists() {
+    ul.remove(artistLi);
 }
